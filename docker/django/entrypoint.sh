1d22c314bbd35b4a55f1e34a4b195c85a692023c
Larry Shaffer <lshaffer@boundlessgeo.com>
2018-07-19 11:13:50 -0600

    Switch docker django setup to entrypoint.sh; inits db on launch

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+### Setup django
+find /code -type f -name '*.pyc' -delete
+[ -d /tmp/static ] || mkdir /tmp/static
+
+manage='python /code/manage.py'
+setup='python /code/setup.py'
+# let the db intialize
+sleep 5
+#$setup build_sphinx
+
+until ${manage} migrate --noinput; do
+  >&2 echo "db is unavailable - sleeping"
+  sleep 5
+done
+${manage} collectstatic --noinput
+${manage} makemigrations --dry-run --verbosity 3
+
+pip freeze
+echo "Dev is set to ${DEV}"
+
+# Run django
 if [[ $DEV == True ]]; then
   python manage.py runserver 0.0.0.0:8808
 else
